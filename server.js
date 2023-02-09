c481af1aff0b8155c607355e94aea21ec96c2d18
Caleb Hodges <chodgesdev@gmail.com>
2023-02-09 14:01:36 -0600

    removed cors from server file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.static(`${__dirname}/public`))
 
 app.get('/api/cat', (req, res) => res.send('barb'))
